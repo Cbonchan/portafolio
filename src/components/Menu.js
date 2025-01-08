9043ddb2e51c1e7e55e880f1a4091895a0af4560
Cbonchan <basterrasebastian@gmail.com>
2025-01-08 15:46:57 -0300

    cambio imagenes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -21,31 +21,31 @@ const Menu = () => {
               name="Weather App"
               description="A page that was purely code in JS, html, CSS. It uses API manadgement to show the weather of a city in real time."
               link="https://cbonchan.github.io/Weather-Page/"
-              image="images/clima.jpg"
+              image="/images/clima.jpg"
             />
             <Project
               name="Gold-hunt Game"
               description="Its a point and click game, It can be played only by installing it in your local machine. I was in charge of the API and database development, and some parts of the game logic."
               link="https://github.com/Cbonchan/gold-hunt"
-              image="images/duckgame.jpg"
+              image="/images/duckgame.jpg"
             />
             <Project
               name="Balatro"
               description="A recreated version of the game following the principles of Object-Oriented Programming (OOP), we used Java and JavaFX for the development. Currently, the game can only be executed directly from the development environment by pressing the 'Run' button in the IDE. "
               link="https://github.com/Cbonchan/Balatro"
-              image="images/balatro.jpg"
+              image="/images/balatro.jpg"
             />
             <Project
               name="Binary Search Tree"
               description="A program written in C with the purpose of creating a Binary Search Tree (BST) and performing operations on it. The program is executed in the terminal and the user can interact with it through the console."
               link="https://github.com/Cbonchan/tda-abb/tree/entrega"
-              image="images/arbol.jpg"
+              image="/images/arbol.jpg"
             />
             <Project
               name="Hash Table"
               description="Also written in C, is a closed hash table with open addressing. The program is executed in the terminal and the user can interact with it through the console."
               link="https://github.com/Cbonchan/tda-abb/tree/entrega"
-              image="images/hash.jpg"
+              image="/images/hash.jpg"
             />
           </div>
         </div>
